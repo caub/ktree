9e3d65287dbc0c8b4c8405c2d7bff4d0ca08d664
caub <cyril.auburtin@gmail.com>
2018-04-14 11:41:13 +0200

    fix closest with neighbors

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 ## Find efficiently the closest hex color
 
 ```js
-import colorNames  from 'color-names';
-import ColorTree from 'color-octree';
+import colorNames from 'color-names';
+import { add, closest } from 'color-octree';
 
 // we expect an array of {hex, name}
-const entries = Object.entries(colorNames).map(([hex, name]) => ({hex, name}));
+const colors = Object.entries(colorNames).map(([hex, name]) => ({ hex, name }));
 
-const tree = new ColorTree();
-tree.addAll(entries);
-console.log(tree.closest('5544df'))
+add(colors);
+console.log(closest('5544df'));
 ```
+
+[live example](https://repl.it/@caub/closest-color)
--- index.js
+++ index.js
@@ -1,72 +1,123 @@
-const parseHex = hex => hex.length < 6 ? hex[hex.length-3].repeat(2)+hex[hex.length-2].repeat(2)+hex[hex.length-1].repeat(2) : hex.slice(-6);
-
-const dist = (c1, c2) => (c1[0]-c2[0])**2 + (c1[1]-c2[1])**2 + (c1[2]-c2[2])**2;
+export const parseHex = hex =>
+  hex.length < 6
+    ? hex[hex.length - 3].repeat(2) + hex[hex.length - 2].repeat(2) + hex[hex.length - 1].repeat(2)
+    : hex.slice(-6);
 
 const hexToRgb = hex => {
   const h = parseHex(hex);
-  return Array.from({length:3}, (_,i)=>parseInt(h.slice(2*i, 2*i+2), 16));
-}
+  return Array.from({ length: 3 }, (_, i) => parseInt(h.slice(2 * i, 2 * i + 2), 16));
+};
+const distRgb = (c1, c2) => (c1[0] - c2[0]) ** 2 + (c1[1] - c2[1]) ** 2 + (c1[2] - c2[2]) ** 2;
 
-const hexToOctants = hex => {
-  const h = parseHex(hex);
-  const [r,g,b] = Array.from({length:3}, (_,i)=>parseInt(h.slice(2*i, 2*i+2), 16).toString(2).padStart(8,0))
-  return Array.from({length:8}, (_,i) => r[i]+g[i]+b[i]);
-}
+export const distHex = (a, b) => distRgb(hexToRgb(a), hexToRgb(b));
 
+const rgbToBin = rgb => rgb.map(x => x.toString(2).padStart(8, 0));
 
-export default class ColorOctant {
-  constructor(path = []) {
-    this.path = path;
-    this.colors = [];
+export const hexToBin = hex => rgbToBin(hexToRgb(hex));
 
-    if (!path.length) {
-      this.closest = hex => {
-        const octants = hexToOctants(hex);
-        let child = this;
-        for (const octant of octants) {
-          if (!child[octant] || !child[octant].colors.length) break;
-          
-          child = child[octant]
+const buildOctree = (depth, n = 0) =>
+  n > depth
+    ? undefined
+    : {
+        n,
+        colors: [],
+        '000': buildOctree(depth, n + 1),
+        '001': buildOctree(depth, n + 1),
+        '010': buildOctree(depth, n + 1),
+        '011': buildOctree(depth, n + 1),
+        '100': buildOctree(depth, n + 1),
+        '101': buildOctree(depth, n + 1),
+        '110': buildOctree(depth, n + 1),
+        '111': buildOctree(depth, n + 1),
+      };
+
+let root;
+
+export const init = () => {
+  root = buildOctree(7); // 8 would go down to leaves, but it's too intense for nodejs
+};
+
+export const add = cols => {
+  if (!root) init();
+  if (Array.isArray(cols)) {
+    return cols.forEach(c => _add(c)); // might want to precmpute rgb to spped up search
+  }
+  _add(cols);
+};
+
+const _add = col => {
+  let node = root;
+  const bin = hexToBin(col.hex);
+  for (let i = 0; i < 7; i++) {
+    const k = bin[0][i] + bin[1][i] + bin[2][i];
+    node = node[k];
+    node.colors.push(col);
+  }
+};
+
+/**
+ * get neighbors of a given node
+ * example: neighbors(['0010', '1101', '0101'], '000') will get the 7 neighbors of this node next the firsr corner ('000')
+ */
+const neighbors = ([r, g, b], dir) => {
+  const rDec = parseInt(r, 2);
+  const gDec = parseInt(g, 2);
+  const bDec = parseInt(b, 2);
+  const nodes = [];
+  const n = r.length;
+  for (let dr = dir[0] - 1; dr <= dir[0]; dr++) {
+    for (let dg = dir[1] - 1; dg <= dir[1]; dg++) {
+      for (let db = dir[2] - 1; db <= dir[2]; db++) {
+        const R = rDec + dr,
+          G = gDec + dg,
+          B = bDec + db;
+        if (R >= 0 && R < 2 ** n && G >= 0 && G < 2 ** n && B >= 0 && B < 2 ** n) {
+          nodes.push([
+            R.toString(2).padStart(n, 0),
+            G.toString(2).padStart(n, 0),
+            B.toString(2).padStart(n, 0),
+          ]);
         }
-
-        if (child.colors.length === 1) return child.colors[0];
-        const targ = hexToRgb(hex)
-        let maxDist = Infinity, closest;
-        child.colors.forEach(col => {
-          const c = hexToRgb(col.hex);
-          const d = dist(c, targ);
-          if (d < maxDist) {
-            maxDist = d;
-            closest = col;
-          }
-        });
-        return Object.assign({d: maxDist}, closest);
       }
     }
   }
+  return nodes;
+};
 
-  addAll(colors) {
-    colors.forEach(({hex, name}) => this.add({hex, name, octants: hexToOctants(hex)}));
-  }
+const getNodeFromCoords = ([r, g, b]) => [...r].reduce((node, ri, i) => node[ri + g[i] + b[i]], root);
 
-  add(color) {
-    if (this.path.length >= 8 || !this.colors.length) {
-      this.colors.push(color);
-      return;
+export const closest = hex => {
+  const rgb = hexToRgb(hex);
+  const [r, g, b] = rgbToBin(rgb);
+  // reminder: we don't/can't store level 8
+  for (let i = 7; i > 0; i--) {
+    const coords = [r.slice(0, i), g.slice(0, i), b.slice(0, i)]; // take one resolution higher, since we don't/can't store level 8
+    const ns = neighbors(coords, r[i] + g[i] + b[i]);
+    const colors = ns.reduce((cs, n) => cs.concat(getNodeFromCoords(n).colors), []);
+    if (colors.length) {
+      return closestIn(rgb, colors);
     }
-    // else we need to go deeper
-    // get first octant of each color, and put them in corresponding child for this octant
-    this.colors.concat(color).forEach(({hex, name, octants: [octant, ...rest]}) => {
-      const child = this.child(octant);
-      child.add({hex, name, octants: rest});
-    });
   }
-
-  child(octant) {
-    if (this[octant]) return this[octant];
-    
-    this[octant] = new ColorOctant(this.path.concat(octant));
-    return this[octant];
+  // search in all
+  const colors = ['000', '001', '010', '011', '100', '101', '110', '111'].reduce(
+    (cs, c) => cs.concat(root[c].colors),
+    [],
+  );
+  if (colors.length) {
+    return closestIn(rgb, colors);
   }
-}
+  console.log(`Couldn't find any color`);
+};
 
+const closestIn = (rgb, colors) => {
+  let minDist = Infinity,
+    best;
+  colors.forEach(col => {
+    const d = distRgb(hexToRgb(col.hex), rgb);
+    if (d < minDist) {
+      minDist = d;
+      best = col;
+    }
+  });
+  return { ...best, d: minDist };
+};
